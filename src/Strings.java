1dd2c84bbd999e3cedd6ac98ce8f21fd9064b35c
unknown <esmajic@yahoo.com>
2019-05-28 16:43:25 +0200

    created method for counting upper case letters and tested it

--- src/Strings.java
+++ src/Strings.java
@@ -8,5 +8,15 @@ public class Strings {
 
 	}
 	
+	public int numberOfUppercaseLetters(String word) {
+		int counter1 = 0;
+		for (int i = 0; i < word.length(); i++) {
+			if (Character.isUpperCase(word.charAt(i))) {
+				counter1++;
+			}
+		}
+		return counter1;
+
+	}
 	
 }
