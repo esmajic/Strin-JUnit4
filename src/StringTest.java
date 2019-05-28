a8730490fac990ae9136a07a480b0d0c366747fa
unknown <esmajic@yahoo.com>
2019-05-28 15:32:11 +0200

    created test method for string length method

--- src/StringTest.java
+++ src/StringTest.java
@@ -1,12 +1,21 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringTest {
+	
+	String word;
+	
+	@Before
+	public void setUp() {
+		word = new String();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void shouldBeGreenWhenStringLengthIsCorrect() {
+		int result = word.stringLength("Ervin");
+		assertEquals(5, result);
 	}
 
 }
